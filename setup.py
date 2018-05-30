b748b0c27347c73966000f90c69ce8c728eaa527
Diego Pomares <github@diegopomares.com>
2018-05-30 22:01:23 +0200

    Separated testing from setup.py
    Coverage was failing to include imports and definitions when running from setup.py

--- setup.py
+++ setup.py
@@ -1,75 +1,16 @@
 import os
 import sys
 
-import multiprocessing
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 import sargeparse as package
 
 
-class PyTest(TestCommand):
-    user_options = [
-        ('no-lint', 'L', "Skip file linting"),
-        ('no-coverage', 'C', "Skip coverage report"),
-        ('test-expression=', 'k', 'Only run tests which match the given substring expression'),
-        ('env-file=', 'e', 'Environment file to load')
-    ]
-
-    # pylint: disable=attribute-defined-outside-init
-    def initialize_options(self):
-        super().initialize_options()
-
-        self.pytest_args = [
-            '--verbose',
-        ]
-        self.lint_args = [
-            '--flake8',
-            '--pylint',
-            '--pylint-jobs={}'.format(multiprocessing.cpu_count())
-        ]
-        self.coverage_args = [
-            '--cov={}'.format(package.__name__),
-            '--cov-report=term-missing',
-            '--cov-report=xml:reports/coverage.xml',
-            '--cov-report=html:reports/coverage',
-        ]
-
-        self.no_lint = False
-        self.no_coverage = False
-        self.test_expression = False
-        self.env_file = None
-
-    def finalize_options(self):
-        super().finalize_options()
-
-        if self.no_lint is False:
-            self.pytest_args.extend(self.lint_args)
-
-        if self.no_coverage is False:
-            self.pytest_args.extend(self.coverage_args)
-
-        if self.test_expression:
-            self.pytest_args.extend(['-k', self.test_expression])
-
-        if self.env_file:
-            set_environment(self.env_file)
-
+class CustomTestCommand(TestCommand):
     def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
-def set_environment(path):
-    with open(path) as f:
-        for line in f:
-            env_vars = [line.lstrip().rstrip('\n') for line in f]
-            env_vars = [line for line in env_vars if line and not line.startswith('#')]
-
-    for line in env_vars:
-        name, value = line.split('=', 2)
-        os.environ[name] = value
+        print("Error: Use script/test-local or scripy/citest for running tests")
+        sys.exit(2)
 
 
 def get_requirements(path):
@@ -83,10 +24,7 @@ def get_requirements(path):
     return requires
 
 
-# install_reqs = get_requirements("requirements/requirements.txt")
-test_reqs = get_requirements("requirements/test_requirements.txt")
-
-
+install_reqs = get_requirements("requirements/requirements.txt")
 setup(
     python_requires='>=3.4',
     name=package.__name__,
@@ -97,8 +35,7 @@ setup(
     version=package.__version__,
     packages=find_packages(),
     include_package_data=True,
-    # install_requires=install_reqs,
-    tests_require=test_reqs,
-    cmdclass={'test': PyTest},
+    install_requires=install_reqs,
+    cmdclass={'test': CustomTestCommand},
     zip_safe=False,
 )
---
+++ test/run.py
@@ -0,0 +1,65 @@
+import os
+import multiprocessing
+import argparse
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir('{}/..'.format(script_path))
+
+PACKAGE = 'sargeparse'
+
+arg_groups = {
+    'default': [
+        '--verbose',
+    ],
+    'lint': [
+        '--flake8',
+        '--pylint',
+        '--pylint-jobs={}'.format(multiprocessing.cpu_count()),
+    ],
+    'coverage': [
+        '--cov={}'.format(PACKAGE),
+        '--cov-report=term-missing',
+        '--cov-report=xml:reports/coverage.xml',
+        '--cov-report=html:reports/coverage',
+    ],
+}
+
+
+def set_environment(path):
+    with open(path) as f:
+        for line in f:
+            env_vars = [line.lstrip().rstrip('\n') for line in f]
+            env_vars = [line for line in env_vars if line and not line.startswith('#')]
+
+    for line in env_vars:
+        name, value = line.split('=', 2)
+        os.environ[name] = value
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-L', '--no-lint', action='store_false', default=True,
+                    dest='lint', help="Skip file linting")
+parser.add_argument('-C', '--no-cov', action='store_false', default=True,
+                    dest='coverage', help="Skip coverage report")
+parser.add_argument('-e', '--env-file', nargs=1,
+                    help="File to load environment variables from")
+
+args, rest = parser.parse_known_args()
+
+pylint_args = ['pytest']
+pylint_args.extend(arg_groups['default'])
+
+if args.lint:
+    pylint_args.extend(arg_groups['lint'])
+
+if args.coverage:
+    pylint_args.extend(arg_groups['coverage'])
+else:
+    pylint_args.append('--no-cov')
+
+pylint_args.extend(rest)
+
+if args.env_file:
+    set_environment(args.env_file)
+
+os.execvp(pylint_args[0], pylint_args)
