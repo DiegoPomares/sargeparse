48f6e2b045d2af03dbbac875b6e45cda793052a2
Diego Pomares <github@diegopomares.com>
2018-06-08 10:46:26 +0200

    Sentinel evaluates to 0 during addition

--- test/unit/test_consts.py
+++ test/unit/test_consts.py
@@ -4,3 +4,5 @@ from sargeparse.consts import _sentinel_factory
 def test_sentinel_factory():
     test = _sentinel_factory('TEST')
     assert test.__repr__() == '<sargeparse.TEST>'
+    assert test + 10 == 10
+    assert 100 + test == 100
