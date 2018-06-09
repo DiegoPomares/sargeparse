c11ce92e54036dca9c24fc694efb25cb00ac45fc
Diego Pomares <github@diegopomares.com>
2018-06-09 14:45:58 +0200

    Improved const test

--- test/unit/test_consts.py
+++ test/unit/test_consts.py
@@ -1,8 +1,23 @@
-from sargeparse.consts import _sentinel_factory
+import sargeparse.consts
+from itertools import permutations
 
 
 def test_sentinel_factory():
-    test = _sentinel_factory('TEST')
+    test = sargeparse.consts._sentinel_factory('TEST')
     assert test.__repr__() == '<sargeparse.TEST>'
     assert test + 10 == 10
     assert 100 + test == 100
+
+    for value in (0, 0.0, False, 1, 1.0, True):
+        assert test != value
+        assert value != test
+
+
+def test_equality():
+    sentinels = (sargeparse.consts.unset, sargeparse.consts.die, sargeparse.consts.stop)
+
+    for sentinel in sentinels:
+        assert sentinel == sentinel(10)
+
+    for sentinel1, sentinel2 in permutations((sentinels), 2):
+        assert sentinel1 != sentinel2
