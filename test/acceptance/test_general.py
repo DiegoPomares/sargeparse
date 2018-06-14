7ba53df7d0a52eb9c062e93f464bef52b21b5f72
Diego Pomares <github@diegopomares.com>
2018-06-14 13:47:54 +0200

    Reorg

--- test/acceptance/test_general.py
+++ test/acceptance/test_general.py
@@ -2,11 +2,8 @@
 import os
 import sys
 import shlex
-import re
 
 from collections import ChainMap
-
-import pytest
 
 import sargeparse
 
@@ -63,6 +60,7 @@ def test_full_ok(caplog):
     parser.add_arguments({
         'names': ['-a', '--arg'],
         'help': 'ARG_HELP',
+        'config_path': 1,
         'default': 'argdef',
     }, {
         'names': ['-b', '--barg'],
@@ -92,6 +90,7 @@ def test_full_ok(caplog):
     def get_config(_args):
         return {
             'boss': 'configboss',
+            1: 'configarg',
             'args': {
                 'barg': 'configbarg'
             }
@@ -114,6 +113,7 @@ def test_full_ok(caplog):
             'boss': 'ENVBOSS',
         },
         {
+            'arg': 'configarg',
             'barg': 'configbarg',
             'boss': 'configboss',
         },
@@ -186,11 +186,11 @@ def test_envvar_default_config_same_name_many_subcommands():
             'confB': '3B',
         }
 
-    sys.argv = shlex.split('test suba')
+    argv = shlex.split('suba')
     os.environ['VARA'] = '1A'
     os.environ['VARB'] = '1B'
 
-    args = parser.parse(read_config=get_config)
+    args = parser.parse(argv=argv, read_config=get_config)
 
     assert args == ChainMap(
         {},
@@ -208,116 +208,4 @@ def test_envvar_default_config_same_name_many_subcommands():
             'arg1': sargeparse.unset,
             'arg2': sargeparse.unset,
         }
-    )
-
-
-def test_show_default_and_help(capsys):
-
-    class CustomObject:
-        def __init__(self, value):
-            self.value = value
-
-        def __repr__(self):
-            return '<CustomObject {}>'.format(self.value)
-
-        def __str__(self):
-            return 'custom_object -> {}'.format(self.value)
-
-        def __eq__(self, other):
-            if isinstance(other, CustomObject):
-                return self.value == other.value
-
-            return super().__eq__(other)
-
-    parser = sargeparse.Sarge({
-        'arguments': [
-            {
-                'names': ['--arg1'],
-                'default': 1,
-                'type': str,
-                'show_default': True,
-            },
-            {
-                'names': ['--arg2'],
-                'default': '2',
-                'type': int,
-                'show_default': True,
-            },
-            {
-                'names': ['--arg3'],
-                'default': '3',
-                'help': None,
-                'show_default': True,
-            },
-            {
-                'names': ['--arg4'],
-                'default': 4,
-                'help': '',
-                'show_default': True,
-            },
-            {
-                'names': ['--arg5'],
-                'default': 5,
-                'help': sargeparse.suppress,
-                'show_default': True,
-            },
-            {
-                'names': ['--arg6'],
-                'default': 6,
-                'help': None,
-                'type': CustomObject,
-                'show_default': True,
-            },
-            {
-                'names': ['--arg7'],
-                'default': CustomObject(7),
-                'type': repr,
-                'help': None,
-                'show_default': True,
-            },
-        ],
-    })
-
-    with pytest.raises(SystemExit) as ex:
-        sys.argv = shlex.split('test -h')
-        parser.parse()
-
-    assert ex.value.code == 0
-
-    captured = capsys.readouterr()
-    assert re.search(r'^ +--arg1.*(?<=WARNING).*(default: 1)', captured.out, re.MULTILINE)
-    assert re.search(r'^ +--arg2.*(?<=WARNING).*(default: 2)', captured.out, re.MULTILINE)
-    assert re.search(r'^ +--arg3.*(?<!WARNING).*(default: 3)', captured.out, re.MULTILINE)
-    assert re.search(r'^ +--arg4.*(?<!WARNING).*(default: 4)', captured.out, re.MULTILINE)
-    assert re.search(r'^ +--arg5', captured.out, re.MULTILINE) is None
-    assert re.search(r'^ +--arg6.*(default: 6)', captured.out, re.MULTILINE)
-    assert re.search(r'^ +--arg7.*(default: custom_object -> 7)', captured.out, re.MULTILINE)
-
-    sys.argv = shlex.split('test')
-    args = parser.parse()
-
-    assert args == ChainMap(
-        {},
-        {},
-        {},
-        {},
-        {
-            'arg1': '1',
-            'arg2': 2,
-            'arg3': '3',
-            'arg4': 4,
-            'arg5': 5,
-            'arg6': CustomObject(6),
-            'arg7': '<CustomObject 7>',
-
-        },
-        {
-            'arg1': sargeparse.unset,
-            'arg2': sargeparse.unset,
-            'arg3': sargeparse.unset,
-            'arg4': sargeparse.unset,
-            'arg5': sargeparse.unset,
-            'arg6': sargeparse.unset,
-            'arg7': sargeparse.unset,
-        },
     )
