1de2911c5cc03f918a2af0fadf72228a4c2a1496
Diego Pomares <github@diegopomares.com>
2018-06-14 15:04:41 +0200

    More argument tests

--- test/integration/test_argument_errors.py
+++ test/integration/test_argument_errors.py
@@ -24,3 +24,71 @@ def test_invalid_config_path():
             sargeparse.Sarge(definition)
 
         assert 'can only be <str> or <list of str>' in str(ex)
+
+
+def test_no_names():
+    definition = {
+        'arguments': [
+            {
+                'help': 'ARGUMENT',
+            }
+        ],
+    }
+
+    with pytest.raises(TypeError) as ex:
+        sargeparse.Sarge(definition)
+
+    assert 'missing or invalid' in str(ex)
+
+
+def test_positional_argument_with_dest():
+    definition = {
+        'arguments': [
+            {
+                'names': ['arg'],
+                'dest': 'argument',
+            }
+        ],
+    }
+
+    with pytest.raises(TypeError) as ex:
+        sargeparse.Sarge(definition)
+
+    assert "Positional arguments cannot have a 'dest'" in str(ex)
+
+
+def test_global_positional_argument():
+    definition = {
+        'arguments': [
+            {
+                'names': ['arg'],
+                'global': True,
+            }
+        ],
+    }
+
+    with pytest.raises(TypeError) as ex:
+        sargeparse.Sarge(definition)
+
+    assert "Positional arguments cannot be 'global'" in str(ex)
+
+
+def test_global_subcommand_argument():
+    definition = {
+        'subcommands': [
+            {
+                'name': 'sub',
+                'arguments': [
+                    {
+                        'names': ['arg'],
+                        'global': True,
+                    }
+                ],
+            },
+        ],
+    }
+
+    with pytest.raises(TypeError) as ex:
+        sargeparse.Sarge(definition)
+
+    assert "Subcommands' arguments cannot be 'global'" in str(ex)
