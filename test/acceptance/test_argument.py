cccbf5f1a484812922c00c8bca462d7f9e40dc85
Diego Pomares <github@diegopomares.com>
2018-06-22 13:32:16 +0200

    Better nargs handling (#22)
    * Nargs/default checks
    
    * Added tests
    
    * Added sargparse.remainder
    
    * Added extra case

---
+++ test/acceptance/test_argument.py
@@ -0,0 +1,213 @@
+# pylint: disable=redefined-outer-name
+import sys
+import shlex
+
+from collections import ChainMap
+
+import sargeparse
+
+
+def test_int_type_no_nargs():
+    definition = {
+        'arguments': [
+            {
+                'names': ['--arg'],
+                'type': int,
+            }
+        ],
+    }
+
+    parser = sargeparse.Sarge(definition)
+    sys.argv = shlex.split('test --arg 10')
+
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {
+            'arg': 10,
+        },
+        {},
+        {},
+        {},
+        {
+            'arg': sargeparse.unset,
+        },
+    )
+
+
+def test_int_type_one_nargs():
+    definition = {
+        'arguments': [
+            {
+                'names': ['--arg'],
+                'nargs': 1,
+                'type': int,
+            }
+        ],
+    }
+
+    parser = sargeparse.Sarge(definition)
+    sys.argv = shlex.split('test --arg 10')
+
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {
+            'arg': [10],
+        },
+        {},
+        {},
+        {},
+        {
+            'arg': sargeparse.unset,
+        },
+    )
+
+
+def test_int_type_many_nargs():
+    definition = {
+        'arguments': [
+            {
+                'names': ['--arg'],
+                'nargs': 2,
+                'default': [100, 200],
+                'type': int,
+            }
+        ],
+    }
+
+    parser = sargeparse.Sarge(definition)
+    sys.argv = shlex.split('test --arg 10 20')
+
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {
+            'arg': [10, 20],
+        },
+        {},
+        {},
+        {},
+        {
+            'arg': sargeparse.unset,
+        },
+    )
+
+    sys.argv = shlex.split('test')
+
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {},
+        {},
+        {},
+        {
+            'arg': [100, 200],
+        },
+        {
+            'arg': sargeparse.unset,
+        },
+    )
+
+
+def test_int_type_remainder():
+    definition = {
+        'arguments': [
+            {
+                'names': ['--arg'],
+                'nargs': sargeparse.remainder,
+                'default': [100, 200],
+                'type': int,
+            }
+        ],
+    }
+
+    parser = sargeparse.Sarge(definition)
+    sys.argv = shlex.split('test --arg 10 20')
+
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {
+            'arg': [10, 20],
+        },
+        {},
+        {},
+        {},
+        {
+            'arg': sargeparse.unset,
+        },
+    )
+
+    sys.argv = shlex.split('test')
+
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {},
+        {},
+        {},
+        {
+            'arg': [100, 200],
+        },
+        {
+            'arg': sargeparse.unset,
+        },
+    )
+
+def test_list_default_no_nargs():
+    def type_fn(value):
+        if isinstance(value, str):
+            return int(value) * 10
+
+        return 10
+
+    definition = {
+        'arguments': [
+            {
+                'names': ['--arg'],
+                'default': [1, 2],
+                'type': type_fn,
+            }
+        ],
+    }
+
+    parser = sargeparse.Sarge(definition)
+
+    sys.argv = shlex.split('test')
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {
+            'arg': 10,
+        },
+        {},
+        {},
+        {},
+        {
+            'arg': sargeparse.unset,
+        },
+    )
+
+    sys.argv = shlex.split('test --arg 3')
+    args = parser.parse()
+
+    assert args == ChainMap(
+        {},
+        {
+            'arg': 30,
+        },
+        {},
+        {},
+        {},
+        {
+            'arg': sargeparse.unset,
+        },
+    )
--- test/integration/test_argument_errors.py
+++ test/integration/test_argument_errors.py
@@ -1,6 +1,5 @@
 # pylint: disable=redefined-outer-name
-import sys
-import shlex
+import re
 
 import pytest
 
@@ -15,8 +14,6 @@ def test_invalid_config_path():
             }
         ],
     }
-
-    sys.argv = shlex.split('test')
 
     for path in (1, [], {}, [1], ['arg', 0]):
         definition['arguments'][0]['config_path'] = path
@@ -92,3 +89,29 @@ def test_global_subcommand_argument():
         sargeparse.Sarge(definition)
 
     assert "Subcommands' arguments cannot be 'global'" in str(ex)
+
+
+def test_nargs_default_mismatch(caplog):
+    definition = {
+        'arguments': [
+            {
+                'names': ['--arg'],
+                'default': 'value',
+            }
+        ],
+    }
+
+    for v in ('*', '+', 1, 2, sargeparse.remainder):
+        with pytest.raises(TypeError) as ex:
+            definition['arguments'][0]['nargs'] = v
+            sargeparse.Sarge(definition)
+
+        assert "'default' must be a list" in str(ex)
+
+    definition['arguments'][0].pop('nargs')
+
+    for v in ((1,), [1], {1}, {1: 2}):
+        definition['arguments'][0]['default'] = v
+        sargeparse.Sarge(definition)
+
+        assert re.search('{}.*{}.*'.format(r'.*arg.*default', type(v)), caplog.text)
